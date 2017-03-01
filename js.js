0296f00c8fc0be9f892d8e43ed7b8707991af129
Alexander Raschid <alexraschid1337@gmail.com>
2017-03-01 23:04:48 +0000

    Initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    //In key, client_id, client_secret, and version(YYYYMMDD) are all pameters. New parameter should me + & 
+    var api= "https://api.foursquare.com/v2/venues/search?client_id=KVHBKSBUSENBBY0EIYOVW2V5ADDHMG3X33MI4EDSF2KP5JLA&client_secret=JC0F3TAWFDENJFGQPFO33GMWQSX2MY01TMAMNUKITYWPGMJN&v=20170301";
+    
+    $("#button").click(function{});
+    
+   // $.get(api + "&near=" + ,);
+    
+})
