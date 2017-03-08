c770f37ea30a242ae5ea777732ac7c899475b863
Alexander Raschid <alexraschid1337@gmail.com>
2017-03-08 21:46:55 +0000

    Got store name to pop uo

--- js.js
+++ js.js
@@ -2,8 +2,27 @@ $(document).ready(function(){
     //In key, client_id, client_secret, and version(YYYYMMDD) are all pameters. New parameter should me + & 
     var api= "https://api.foursquare.com/v2/venues/search?client_id=KVHBKSBUSENBBY0EIYOVW2V5ADDHMG3X33MI4EDSF2KP5JLA&client_secret=JC0F3TAWFDENJFGQPFO33GMWQSX2MY01TMAMNUKITYWPGMJN&v=20170301";
     
-    $("#button").click(function{});
-    
-   // $.get(api + "&near=" + ,);
+    $('button').on('click', function(event) {
+
+
+
+        var food = $('#inputFood').val();
+        var location = $('#inputLocation').val();
+        var URL = "https://api.foursquare.com/v2/venues/search?"+
+        "client_id=KVHBKSBUSENBBY0EIYOVW2V5ADDHMG3X33MI4EDSF2KP5JLA&"+
+    "client_secret=JC0F3TAWFDENJFGQPFO33GMWQSX2MY01TMAMNUKITYWPGMJN&v=20170301&"+
+        "near="+ location +
+        "&query=" + food;
+
+        $.get(URL, function(data) {
+            var venues = data.response.venues;
+            var myresult = "";
+            for (var i = 0; i < 10; i++) {
+                myresult += "<li>NAME: " + venues[i].name; "</li>"
+            }
+            $("#result").html(myresult);
+        })
+    });
+
     
 })
